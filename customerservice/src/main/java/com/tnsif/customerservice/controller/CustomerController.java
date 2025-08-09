4b2ce8125490733a253753139b43cb04bef6e175
Thv0529 <thamizharasi415@gmail.com>
2025-08-09 15:34:36 +0530

    C2TC_SpringBoot Project

---
+++ customerservice/src/main/java/com/tnsif/customerservice/controller/CustomerController.java
@@ -0,0 +1,48 @@
+package com.tnsif.customerservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tnsif.customerservice.entity.Customer;
+import com.tnsif.customerservice.repository.CustomerRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class CustomerController {
+	@Autowired
+	
+	private CustomerRepository repo;
+	
+	//to get all the records from table
+	public List<Customer> listAll(){
+		return repo.findAll();
+		
+	}
+	
+	//save the record
+	public void save(Customer c) {
+		repo.save(c);
+	}
+	
+	
+	//to call a specific record 
+	public Customer get(Integer cid) { 
+		return repo.findById(cid).get(); 
+	}
+	
+	//delete particular record with id
+	public void delete(Integer cid) {
+		repo.deleteById(cid);
+	}
+	
+	//for update
+	public void update(Customer c) {
+		repo.save(c);
+	}
+
+
+}
---
+++ customerservice/src/main/java/com/tnsif/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.tnsif.customerservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.tnsif.customerservice.entity.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
+
